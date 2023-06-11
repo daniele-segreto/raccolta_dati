19fc67f245782a2db2aeb54be8f4d9e5e7a14e75
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2023-06-12 01:05:30 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
-
+import { BackendService } from './services/backend.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
-}
+  userData = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  isFormOpen = false;
+  showSuccessMessage: boolean = false;
+
+  constructor(private backendService: BackendService) { }
+
+  submitForm() {
+    // Logica per l'invio dei dati del form al backend
+    this.backendService.addUser(this.userData).subscribe(
+      (response: any) => {
+        console.log(response); // Puoi gestire la risposta del backend a tua discrezione
+      },
+      (error: any) => {
+        console.error(error); // Puoi gestire l'errore a tua discrezione
+      }
+      );
+
+      // Dopo l'invio con successo, mostra il messaggio di successo
+      this.showSuccessMessage = true;
+
+      // Resetta i valori dei campi di input
+      this.userData = {
+        name: '',
+        email: '',
+        password: ''
+      };
+    }
+
+    toggleForm() {
+      // Logica per aprire/chiudere il Form
+      this.isFormOpen = !this.isFormOpen;
+    }
+
+  }
+
---
+++ src/app/services/backend.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+  private apiUrl = 'http://localhost:8000/api'; // Sostituisci con l'URL del tuo backend Laravel
+
+  constructor(private http: HttpClient) { }
+
+  addUser(userData: any) {
+    return this.http.post(`${this.apiUrl}/users`, userData);
+  }
+}
